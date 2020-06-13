757e6db003c9cbea52ce79645f466a5c3336bca6
Jay Wei <41401372+jayjayjohn@users.noreply.github.com>
2020-06-13 14:54:44 -0400

    Update producer.scala

--- kafka/producer_template/src/main/scala/producer.scala
+++ kafka/producer_template/src/main/scala/producer.scala
@@ -5,47 +5,53 @@ import com.fasterxml.jackson.core.JsonFactory
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
+import com.typesafe.config.ConfigFactory
+
 object kafka_producer {
 
   def main(args: Array[String]): Unit = {
-    println("Kafka Producer Application Started ...")
 
-    val kafka_topic_name = "test"
-    val kafka_bootstrap_servers = "localhost:9092"
+    val conf = ConfigFactory.load
+	// conf.getConfig(args(0)) for input argument  
+    val envProps = conf.getConfig("dev")	  
+	  
+    val kafka_topic_name = envProps.getString("topic")
+    val kafka_bootstrap_servers = envProps.getString("bootstrap.server")
 
     // ==============Streaming HTTP Request to API===================== 
     val meetup_rsvp_stream_api_endpoint = "http://stream.meetup.com/2/rsvps"
 
-	val url_object = new URL(meetup_rsvp_stream_api_endpoint)
-    val connection_object = url_object.openConnection()
+    val url = new URL(meetup_rsvp_stream_api_endpoint)
+    val connection = url.openConnection()
 	
     val jsonfactory_object = new JsonFactory(new ObjectMapper)
-    val parser_object = jsonfactory_object.createParser(connection_object.getInputStream)
+    val json_parser = jsonfactory_object.createParser(connection.getInputStream)
 	// ==============Streaming HTTP Request to API===================== 
 	
 	
-	val properties_object = new Properties()
-    properties_object.put("bootstrap.servers", kafka_bootstrap_servers)
-    properties_object.put("acks", "all")
-    properties_object.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    properties_object.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    properties_object.put("enable.auto.commit", "true")
-    properties_object.put("auto.commit.interval.ms", "1000")
-    properties_object.put("session.timeout.ms", "30000")
+    val props = new Properties()
+    props.put("bootstrap.servers", kafka_bootstrap_servers)
+    props.put("acks", "all")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("enable.auto.commit", "true")
+    props.put("auto.commit.interval.ms", "1000")
+    props.put("session.timeout.ms", "30000")
 
-    val kafka_producer_object = new KafkaProducer[String,String](properties_object)
-    while (parser_object.nextToken() != null)
+    val kafka_producer = new KafkaProducer[String,String](props)
+	  
+    while (json_parser.nextToken() != null)
     {
-      val message_record = parser_object.readValueAsTree().toString()
-      println(message_record)
-      val producer_record_object = new ProducerRecord[String, String](kafka_topic_name, message_record)
+      val message = json_parser.readValueAsTree().toString()
+	    
+      println(message)
+      val producer_record_object = new ProducerRecord[String, String](kafka_topic_name, message)
       
-	  kafka_producer_object.send(producer_record_object)
+	  kafka_producer.send(producer_record_object)
     }
 	
-    kafka_producer_object.close()
+    kafka_producer.close()
 
-    println("Kafka Producer Application Completed.")
 	
 	}
 	}
